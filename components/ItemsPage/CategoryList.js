437731730a084bee5c91df9fad32ed25d3a89fae
Yoni <46503638+yoniakabecky@users.noreply.github.com>
2021-02-19 12:05:14 -0800

    Implement framer motion (#27)

--- components/ItemsPage/CategoryList.js
+++ components/ItemsPage/CategoryList.js
@@ -1,15 +1,15 @@
+import { motion, AnimatePresence } from "framer-motion";
 import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 
-import { getItemsByCategory } from "@@/utils/handlers";
 import TriangleIcon from "../Icons/TriangleIcon";
 import DraggableCard from "../DraggableCard";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  category: {
     position: "relative",
     display: "flex",
     margin: "1rem auto",
@@ -18,41 +18,57 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.primary.main,
     color: "#828282",
   },
-  name: {
-    flexGrow: 1,
-    lineHeight: "unset",
-  },
-  icon: {
-    fill: "#828282",
-  },
+  grow: { flexGrow: 1 },
+  name: { marginTop: 4 },
 }));
 
-export default function CategoryList({ name, id }) {
+export default function CategoryList({ name, items }) {
   const classes = useStyles();
-  const [items, setItems] = React.useState([]);
-
-  const handleClick = async () => {
-    try {
-      const data = await getItemsByCategory(id);
-      setItems(data);
-    } catch (err) {
-      console.error("Failed to get items: ", err);
-    }
-  };
+  const [isOpen, setIsOpen] = React.useState(false);
 
   return (
     <React.Fragment>
-      <Paper className={classes.root} onClick={handleClick}>
-        <Typography variant="h3" className={classes.name}>
-          {name}
-        </Typography>
+      <Paper className={classes.category} onClick={() => setIsOpen(!isOpen)}>
+        <motion.span
+          initial={false}
+          animate={{ color: isOpen ? "#EB5757" : "#828282" }}
+          className={classes.grow}
+        >
+          <Typography variant="h3" className={classes.name}>
+            {name}
+          </Typography>
+        </motion.span>
 
-        <TriangleIcon className={classes.icon} />
+        <motion.span
+          initial={false}
+          animate={{
+            fill: isOpen ? "#EB5757" : "#828282",
+            rotate: isOpen ? 180 : 0,
+          }}
+          transition={{ duration: 0.5 }}
+        >
+          <TriangleIcon />
+        </motion.span>
       </Paper>
 
-      {items.map((item) => (
-        <DraggableCard type="item" {...item} key={`item-${item.id}`} />
-      ))}
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+            variants={{
+              open: { opacity: 1, height: "auto" },
+              collapsed: { opacity: 0, height: 0 },
+            }}
+            transition={{ duration: 0.5, ease: [0.04, 0.62, 0.23, 0.98] }}
+          >
+            {items.map((item) => (
+              <DraggableCard type="item" {...item} key={`item-${item.id}`} />
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </React.Fragment>
   );
 }
