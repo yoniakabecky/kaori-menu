516f7a6c64fb4d65aebc3d4e2c1b26d492c7f407
Yoni <yoniakabecky@gmail.com>
2021-02-25 16:58:12 -0800

    Update authHandlers.js

--- utils/authHandlers.js
+++ utils/authHandlers.js
@@ -36,6 +36,7 @@ const postUserToken = async (token) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({ token: token }),
   });
